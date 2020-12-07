f12d0ced66074a72c57f0417c7262e0d7c124291
Marcos Rabinowicz <marcosrabinowicz@hotmail.com>
2020-12-07 20:32:02 -0300

    Swagger configurado.

--- ProductCatalog/Startup.cs
+++ ProductCatalog/Startup.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ProductCatalog.Data;
 using Microsoft.Extensions.Configuration;
 using ProductCatalog.Repositories;
+using Microsoft.OpenApi.Models;
 
 namespace ProductCatalog
 {
@@ -33,6 +33,11 @@ namespace ProductCatalog
 
             services.AddScoped<StoreDataContext, StoreDataContext>();
             services.AddTransient<ProductRepository, ProductRepository>();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Product Catalog", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +58,12 @@ namespace ProductCatalog
             });
 
             app.UseResponseCompression();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Catalog - v1");
+            });
         }
     }
 }
